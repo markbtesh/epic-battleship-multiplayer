02cb4973273741407d96f01801a2c30ef6c3ee2f
markbtesh <63694092+markbtesh@users.noreply.github.com>
2024-08-31 19:05:19 -0400

    Update app.component.spec.ts

--- battleship-multiplayer/src/app/app.component.spec.ts
+++ battleship-multiplayer/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'battleship-single-player' title`, () => {
+  it(`should have the 'battleship-multiplayer' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('battleship-single-player');
+    expect(app.title).toEqual('battleship-multiplayer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, battleship-single-player');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, battleship-multiplayer');
   });
 });
